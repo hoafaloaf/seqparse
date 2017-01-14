8c011b0bfb47446552a267d94525eb00001c7022
Geoff Harvey <hoafaloaf@gmail.com>
2017-01-13 22:13:30 -0800

    o Added: seqparse.validate_frame_sequence(). o Changed: Seqparse class now reads its own files! o Removed: Windows carriage returns.

--- python/seqparse/__init__.py
+++ python/seqparse/__init__.py
@@ -1,3 +1,18 @@
 """Seqparse: A nifty way to list your file sequences."""
 
 from .seqparse import Seqparse
+
+__all__ = ("get_parser", "validate_frame_sequence")
+
+###############################################################################
+# EXPORTED METHODS
+
+
+def get_parser(*args, **kwargs):
+    """Return a new Seqparse instance."""
+    return seqparse.Seqparse(*args, **kwargs)
+
+
+def validate_frame_sequence(frame_seq):
+    """Whether the supplied frame (not file) sequence is valid."""
+    return seqparse.Seqparse.validate_frame_sequence(frame_seq)
--- python/seqparse/seqparse.py
+++ python/seqparse/seqparse.py
@@ -5,14 +5,12 @@ import os
 import re
 from collections import defaultdict
 
+# Third Party Libraries
+import scandir
+
 from .classes import FileSequence
 
 __all__ = ("Seqparse", )
-
-###############################################################################
-# INTERNAL CONSTANTS
-
-SEQ_EXPR = re.compile("^(?P<base>.*)\.(?P<frame>\d+)\.(?P<ext>[^\.]+)$")
 
 ###############################################################################
 # Class: Seqparse
@@ -21,6 +19,9 @@ SEQ_EXPR = re.compile("^(?P<base>.*)\.(?P<frame>\d+)\.(?P<ext>[^\.]+)$")
 class Seqparse(object):
     """Storage and parsing engine for file sequences."""
 
+    FILE_EXPR = re.compile("(?P<base>.*)\.(?P<frame>\d+)\.(?P<ext>[^\.]+)")
+    SEQ_EXPR = re.compile("(\d+(?:-\d+(?:x\d+)?)?(?:,\d+(?:-\d+(?:x\d+)?)?)*)")
+
     def __init__(self):
         """Initialise the instance."""
         self._seqs = defaultdict(FileSequence)
@@ -28,28 +29,55 @@ class Seqparse(object):
 
     @property
     def sequences(self):
-        """A dictionary of tracked file sequences and singletons."""
+        """A dictionary of tracked file sequences."""
         return self._seqs
+
+    @property
+    def singletons(self):
+        """A set of tracked singleton files."""
+        return self._singletons
+
+    def add_file(self, file_name):
+        """Add a file to the parser instance."""
+        smatch = self.FILE_EXPR.match(str(file_name))
+
+        if smatch:
+            base_name, frame, file_ext = smatch.groups()
+
+            seq = self.sequences[base_name]
+            seq.name = base_name
+
+            ext = seq[file_ext]
+            pad = len(frame)
+            ext[pad].add(frame)
+
+        else:
+            self._singletons.add(file_name)
 
     def add_from_walk(self, root, file_names):
         """Shortcut for adding file sequences from os/scandir.walk."""
         root = str(root)
 
         for file_name in file_names:
-            smatch = SEQ_EXPR.match(str(file_name))
-            if smatch:
-                base_name, frame, file_ext = smatch.groups()
-                base_name = os.path.join(root, base_name)
-
-                seq = self.sequences[base_name]
-                seq.name = base_name
-
-                ext = seq[file_ext]
-                pad = len(frame)
-                ext[pad].add(frame)
+            self.add_file(os.path.join(root, file_name))
 
     def output(self, tree=False):
-        """Return a list of the file sequences contained by the instance."""
+        """Yield a list of contained file sequences and singletons."""
         for base_name, file_seq in sorted(self.sequences.items()):
             for output in file_seq.output():
                 yield output
+
+        for singleton in sorted(self.singletons):
+            yield singleton
+
+    def scan_path(self, search_path):
+        """Scan supplied path, add all discovered files to the instance."""
+        for root, dir_names, file_names in scandir.walk(search_path):
+            self.add_from_walk(root, file_names)
+
+    @classmethod
+    def validate_frame_sequence(frame_seq):
+        """Whether the supplied frame (not file) sequence is valid."""
+        if self.SEQ_EXPR.match(frame_seq):
+            return True
+        return False
