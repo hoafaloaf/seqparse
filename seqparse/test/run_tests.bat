9c964264530cc672d09b5ab50ef2fa4012520573
Geoff Harvey <hoafaloaf@gmail.com>
2017-03-10 15:58:02 -0800

    o Added: Initial setuptools configuration. o Moved: Test suite (and scripts) now live in the seqparse module.

--- seqparse/test/run_tests.bat
+++ seqparse/test/run_tests.bat
@@ -5,15 +5,16 @@ set PYTHONPATH=%~dp0
 set PYTHONPATH=%PYTHONPATH:~0,-1%
 
 for %%A in ("%PYTHONPATH%") do (@set PYTHONPATH=%%~dpA)
-set PYTHONPATH=%PYTHONPATH%python;%pythonpath_bak%
+
+set PYTHONPATH=%PYTHONPATH:~0,-1%
+set PYTHONPATH=%PYTHONPATH%;%pythonpath_bak%
 echo PYTHONPATH: %PYTHONPATH%
 
-nosetests -v -s tests --with-xunit --all-modules --traverse-namespace ^
+nosetests -v -s seqparse --with-xunit --all-modules --traverse-namespace ^
   --cover-erase --cover-html --with-coverage --cover-inclusive --with-id
 
 python -m coverage xml --include=seqparse*
 
-@echo off
 >pylint.out (
   pylint -d I0011,R0204,R0801 seqparse ^
     --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}"
