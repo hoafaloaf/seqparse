86ec4173db804c668500cdd47019fcedcee90ef4
Geoff Harvey <hoafaloaf@gmail.com>
2017-03-21 22:21:19 -0700

    o Changed: Updated version to v0.4.0a3. [ci skip]

--- setup.py
+++ setup.py
@@ -31,5 +31,5 @@ setup(
     test_suite='nose.collector',
     tests_require=['coverage', 'nose', 'pylint'],
     url='http://github.com/hoafaloaf/seqparse',
-    version='0.4.0a2',
+    version='0.4.0a3',
     zip_safe=False)
