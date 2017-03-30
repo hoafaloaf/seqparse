fe50cdcef9ea6810a7a74b2b148e45c67c6887a4
Geoff Harvey <hoafaloaf@gmail.com>
2017-03-30 14:30:41 -0700

    o Added: seqparse.test: Mock os.stat data.

--- seqparse/test/__init__.py
+++ seqparse/test/__init__.py
@@ -7,6 +7,69 @@ __all__ = ("DirEntry", "generate_entries", "initialise_mock_scandir_data",
 
 MOCK_SCANDIR_DEEP_DATA = list()
 
+MOCK_SCANDIR_STAT_DATA = {
+    'test.0001.py': {
+        'm_size': 7975L,
+        'm_uid': 0L,
+        'm_dev': 14L,
+        'm_nlink': 1L,
+        'm_gid': 0L,
+        'm_mode': 33279L,
+        'm_mtime': 1490908305,
+        'm_atime': 1490908305,
+        'm_ino': 12666373952092765L,
+        'm_ctime': 1490908313
+    },
+    'test.0002.py': {
+        'm_size': 987L,
+        'm_uid': 0L,
+        'm_dev': 14L,
+        'm_nlink': 1L,
+        'm_gid': 0L,
+        'm_mode': 33279L,
+        'm_mtime': 1490908305,
+        'm_atime': 1490908305,
+        'm_ino': 2814749767415947L,
+        'm_ctime': 1490908318
+    },
+    'test.0003.py': {
+        'm_size': 2561L,
+        'm_uid': 0L,
+        'm_dev': 14L,
+        'm_nlink': 1L,
+        'm_gid': 0L,
+        'm_mode': 33279L,
+        'm_mtime': 1490908305,
+        'm_atime': 1490908305,
+        'm_ino': 2251799813994636L,
+        'm_ctime': 1490908325
+    },
+    'test.0004.py': {
+        'm_size': 6487L,
+        'm_uid': 0L,
+        'm_dev': 14L,
+        'm_nlink': 1L,
+        'm_gid': 0L,
+        'm_mode': 33279L,
+        'm_mtime': 1490908305,
+        'm_atime': 1490908305,
+        'm_ino': 2251799813994637L,
+        'm_ctime': 1490908333
+    },
+    'test.0006.py': {
+        'm_size': 18510L,
+        'm_uid': 0L,
+        'm_dev': 14L,
+        'm_nlink': 1L,
+        'm_gid': 0L,
+        'm_mode': 33279L,
+        'm_mtime': 1490908305,
+        'm_atime': 1490908305,
+        'm_ino': 2251799813994638L,
+        'm_ctime': 1490908340
+    }
+}
+
 ###############################################################################
 # EXPORTED CLASSES
 
@@ -32,6 +95,17 @@ class DirEntry(object):
     def is_file(self, follow_symlinks=False):  # pylint: disable=W0613
         """Whether this instance represents a file."""
         return self._is_file
+
+    def stat(self, follow_symlinks=False):  # pylint: disable=W0613
+        """Return mock'd os.stat object for the given file."""
+        if self.name not in MOCK_SCANDIR_STAT_DATA:
+            raise IOError("Mock'd file not found: {}".format(self.name))
+
+        mock_stat = object()
+        for attr, val in MOCK_SCANDIR_STAT_DATA.iteritems():
+            setattr(mock_stat, attr, val)
+
+        return mock_stat
 
 
 ###############################################################################
